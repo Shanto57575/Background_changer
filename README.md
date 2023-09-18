ae38cb5e9fbcd5f088e473b7bb9d806d0714fc18
Shanto57575 <shanto57575@gmail.com>
2023-09-18 10:38:48 +0600

    added styles in button

--- README.md
+++ README.md
@@ -1,4 +1,3 @@
 # Background Changer
 
-#FF0000 #008000 #0000FF
 Ex: If you click red button the background color will be red and same goes for every button
--- src/App.jsx
+++ src/App.jsx
@@ -7,64 +7,64 @@ const App = () => {
 		<div
 			className={`${color} h-screen flex items-end justify-center flex-wrap pb-10`}
 		>
-			<div className="bg-slate-700 p-2 rounded-full">
+			<div className="bg-white p-2 rounded-full">
 				<button
 					onClick={() => setColor("bg-red-500")}
-					className="w-20 border-2 px-3 py-2 bg-red-500 text-white rounded-3xl mr-3"
+					className="w-20 border px-3 py-2 bg-red-500 text-white rounded-3xl mr-3 font-semibold font-serif"
 				>
 					Red
 				</button>
 				<button
 					onClick={() => setColor("bg-green-600")}
-					className="w-20 border-2 px-3 py-2 bg-green-600 text-white rounded-3xl mr-3"
+					className="w-20 border px-3 py-2 bg-green-600 text-white rounded-3xl mr-3 font-semibold font-serif"
 				>
 					green
 				</button>
 				<button
 					onClick={() => setColor("bg-blue-500")}
-					className="w-20 border-2 px-3 py-2 bg-blue-500 text-white rounded-3xl mr-3"
+					className="w-20 border px-3 py-2 bg-blue-500 text-white rounded-3xl mr-3 font-semibold font-serif"
 				>
 					blue
 				</button>
 				<button
 					onClick={() => setColor("bg-yellow-400")}
-					className="w-20 border-2 px-3 py-2 bg-yellow-400 text-white rounded-3xl mr-3"
+					className="w-20 border px-3 py-2 bg-yellow-400 text-white rounded-3xl mr-3 font-semibold font-serif"
 				>
 					yellow
 				</button>
 				<button
 					onClick={() => setColor("bg-slate-500")}
-					className="w-20 border-2 px-3 py-2 bg-slate-500 text-white rounded-3xl mr-3"
+					className="w-20 border px-3 py-2 bg-slate-500 text-white rounded-3xl mr-3 font-semibold font-serif"
 				>
 					slate
 				</button>
 				<button
 					onClick={() => setColor("bg-pink-300")}
-					className="w-20 border-2 px-3 py-2 bg-pink-500 text-white rounded-3xl mr-3"
+					className="w-20 border px-3 py-2 bg-pink-500 text-white rounded-3xl mr-3 font-semibold font-serif"
 				>
 					pink
 				</button>
 				<button
 					onClick={() => setColor("bg-lime-400")}
-					className="w-20 border-2 px-3 py-2 bg-lime-400 text-white rounded-3xl mr-3"
+					className="w-20 border px-3 py-2 bg-lime-400 text-white rounded-3xl mr-3 font-semibold font-serif"
 				>
 					lime
 				</button>
 				<button
 					onClick={() => setColor("bg-cyan-500")}
-					className="w-20 border-2 px-3 py-2 bg-cyan-500 text-white rounded-3xl mr-3"
+					className="w-20 border px-3 py-2 bg-cyan-500 text-white rounded-3xl mr-3 font-semibold font-serif"
 				>
 					cyan
 				</button>
 				<button
 					onClick={() => setColor("bg-amber-500")}
-					className="w-20 border-2 px-3 py-2 bg-amber-500 text-white rounded-3xl mr-3"
+					className="w-20 border px-3 py-2 bg-amber-500 text-white rounded-3xl mr-3 font-semibold font-serif"
 				>
 					amber
 				</button>
 				<button
 					onClick={() => setColor("bg-purple-500")}
-					className="w-20 border-2 px-3 py-2 bg-purple-500 text-white rounded-3xl mr-3"
+					className="w-20 border px-3 py-2 bg-purple-500 text-white rounded-3xl mr-3 font-semibold font-serif"
 				>
 					purple
 				</button>
